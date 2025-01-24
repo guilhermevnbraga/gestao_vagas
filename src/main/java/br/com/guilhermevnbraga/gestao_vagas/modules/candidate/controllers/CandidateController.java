45dd8783a1e5e10b351741a34d9eec539d028d46
Guilherme Braga <89932943+guilhermevnbraga@users.noreply.github.com>
2025-01-23 22:13:01 -0300

    feat: Initial commit

---
+++ src/main/java/br/com/guilhermevnbraga/gestao_vagas/modules/candidate/controllers/CandidateController.java
@@ -0,0 +1,27 @@
+package br.com.guilhermevnbraga.gestao_vagas.modules.candidate.controllers;
+
+import br.com.guilhermevnbraga.gestao_vagas.exceptions.UserFoundException;
+import br.com.guilhermevnbraga.gestao_vagas.modules.candidate.CandidateEntity;
+import br.com.guilhermevnbraga.gestao_vagas.modules.candidate.CandidateRepository;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/candidate")
+public class CandidateController {
+  @Autowired private CandidateRepository candidateRepository;
+
+  @PostMapping("/")
+  public CandidateEntity
+  create(@Valid @RequestBody CandidateEntity candidateEntity) {
+    this.candidateRepository
+        .findByUsernameOrEmail(candidateEntity.getUsername(),
+                               candidateEntity.getEmail())
+        .ifPresent((user) -> { throw new UserFoundException(); });
+    return this.candidateRepository.save(candidateEntity);
+  }
+}
