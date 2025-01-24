c2e367c03ef5dc5af638231e02534cf28708b11d
Guilherme Braga <89932943+guilhermevnbraga@users.noreply.github.com>
2025-01-24 17:09:59 -0300

    feat: Add token logic for candidate

--- src/main/java/br/com/guilhermevnbraga/gestao_vagas/modules/candidate/CandidateRepository.java
+++ src/main/java/br/com/guilhermevnbraga/gestao_vagas/modules/candidate/CandidateRepository.java
@@ -9,4 +9,5 @@ import br.com.guilhermevnbraga.gestao_vagas.modules.candidate.entities.Candidate
 public interface CandidateRepository
     extends JpaRepository<CandidateEntity, UUID> {
   Optional<CandidateEntity> findByUsernameOrEmail(String username, String email);
+  Optional<CandidateEntity> findByUsername(String username);
 }
--- src/main/java/br/com/guilhermevnbraga/gestao_vagas/modules/candidate/useCases/CreateCandidateUseCase.java
+++ src/main/java/br/com/guilhermevnbraga/gestao_vagas/modules/candidate/useCases/CreateCandidateUseCase.java
@@ -1,21 +1,27 @@
 package br.com.guilhermevnbraga.gestao_vagas.modules.candidate.useCases;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
 import br.com.guilhermevnbraga.gestao_vagas.exceptions.UserFoundException;
 import br.com.guilhermevnbraga.gestao_vagas.modules.candidate.CandidateRepository;
 import br.com.guilhermevnbraga.gestao_vagas.modules.candidate.entities.CandidateEntity;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
 
 @Service
 public class CreateCandidateUseCase {
   @Autowired private CandidateRepository candidateRepository;
+
+  @Autowired private PasswordEncoder passwordEncoder;
 
   public CandidateEntity execute(CandidateEntity candidateEntity) {
     this.candidateRepository
         .findByUsernameOrEmail(candidateEntity.getUsername(),
                                candidateEntity.getEmail())
         .ifPresent((user) -> { throw new UserFoundException(); });
+
+    var password = passwordEncoder.encode(candidateEntity.getPassword());
+    candidateEntity.setPassword(password);
     return this.candidateRepository.save(candidateEntity);
   }
 }
